940dc847b8e8319e055476fe89d996ed35e9f097
CHЯISΓФPHΞЯ HΞDЯICH <Christopherhedrich@gmail.com>
2025-12-04 19:39:58 -0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { FavoritesProvider } from './src/context/FavoritesContext';
-import Header from './src/component/Header';
-import Home from './src/pages/home';
-import CharacterDetailPage from './src/pages/CharacterDetailpage';
-import FavoritesPage from './src/pages/FavoritesPage';
+import { FavoritesProvider } from './src/context/FavoritesContext.jsx';
+import Header from './src/component/Header.jsx';
+import Home from './src/pages/home.jsx';
+import CharacterDetailPage from './src/pages/CharacterDetailpage.jsx';
+import FavoritesPage from './src/pages/FavoritesPage.jsx';
 import './src/styles/App.css';
 
 function App() {
